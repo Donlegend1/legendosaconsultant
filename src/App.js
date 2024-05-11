dee38b3a1e437deb4134c92683d852d633a827dd
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-05-11 22:22:28 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import Header2 from "./components/Header2";
+
 
 import Footer from "./components/Footer";
-import Slidder from "./components/Slidder";
+
 
 function App() {
 
-   const currentUrl = window.location.pathname;
 
-   console.log("Current URL:", currentUrl);
-
-   // Check if the current URL is the homepage
-   const isHomePage = currentUrl === "/";
   return (
     <Fragment>
      
