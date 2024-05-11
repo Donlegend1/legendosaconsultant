c40d37b9a6e6b583a01ebf2d20a3e2a0ebbec51d
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-05-11 21:47:29 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, { Fragment, useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -12,24 +11,22 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const Loaderimg = () => {
-  return (
-    <div id="global-loader">
-      <img
-        src={"/logo512.png"}
-        className="loader-img"
-        alt="Loader"
-      />
-    </div>
-  );
-};
+
+const Loaderimg = () => (
+  <div id="global-loader">
+    <img
+      src={"/logo512.png"}
+      className="loader-img"
+      alt="Loader"
+    />
+  </div>
+);
+
 root.render(
-  <Fragment>
-   
+  <>
     <BrowserRouter>
-      <React.Suspense fallback={Loaderimg()}>
+      <React.Suspense fallback={<Loaderimg />}>
         <Routes>
-          {/* <Route element={<MainLayout />}></Route> */}
           <Route element={<App />}>
             <Route
               path={`${process.env.PUBLIC_URL}/`}
@@ -55,11 +52,7 @@ root.render(
         </Routes>
       </React.Suspense>
     </BrowserRouter>
-
-  </Fragment>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
