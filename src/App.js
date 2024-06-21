e763589deafd2f933c3c17a94aa0472303e9ac4d
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-06-21 16:33:11 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-
-
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+const Loaderimg = () => (
+  <div id="global-loader">
+    <img
+      src={"/logo512.png"}
+      className="loader-img"
+      alt="Loader"
+    />
+  </div>
+);
 
-
-function App() {
-
-
+const App = () => {
   return (
-    <Fragment>
-     
-        <Header />
-      <Outlet />
-
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path={`${process.env.PUBLIC_URL}/`}
+          element={<Home />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/about`}
+          element={<About />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/services`}
+          element={<Services />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/project`}
+          element={<Projects />}
+        />
+        <Route
+          path={`${process.env.PUBLIC_URL}/contact`}
+          element={<Contact />}
+        />
+      </Routes>
+    
       <Footer />
-    </Fragment>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,60 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-
-
-
-const Loaderimg = () => (
-  <div id="global-loader">
-    <img
-      src={"/logo512.png"}
-      className="loader-img"
-      alt="Loader"
-    />
-  </div>
-);
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  
-    <React.StrictMode>
-      <BrowserRouter>
-        <React.Suspense fallback={<Loaderimg />}>
-          <Routes>
-            <Route element={<App />}>
-              <Route
-                path={`${process.env.PUBLIC_URL}/`}
-                element={<Home />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/about`}
-                element={<About />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/services`}
-                element={<Services />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/project`}
-                element={<Projects />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/contact`}
-                element={<Contact />}
-              />
-            </Route>
-          </Routes>
-        </React.Suspense>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
