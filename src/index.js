c61e12e56a056407d247b383a04136a2a9625928
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-06-23 13:49:15 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import "./index.css";
 
+const App = () => {
+  return (
+    <Fragment>
+      {/* <ContextProvider> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/services"
+            element={<Services />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* </ContextProvider> */}
+    </Fragment>
+  );
+};
+
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
